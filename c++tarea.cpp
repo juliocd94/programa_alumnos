9ebea7107dcfd7c4071b11fb046312d3d2e50885
juliocd94 <77243978+juliocd94@users.noreply.github.com>
2021-10-24 10:19:22 -0430

    ajuste

--- c++tarea.cpp
+++ c++tarea.cpp
@@ -137,12 +137,10 @@ cout<<"--------------------------------------------------------------"<<endl;
 	if(hayregistros7 == 0 & hayregistros8 == 0 & hayregistros9 == 0){
 		cout<<endl<<endl<<"NO HAY REGISTROS PARA MOSTRAR";
 	}else{
-		cout<<endl<<"Que operacion de sea realizar: "<<endl;
-		cout<<"Presione 1 para mostrar resultados ";
+		cout<<endl<<"Presione 1 para mostrar resultados "<<endl;
 		cin>>operacion;
 		if(operacion ==1){
 			if(hayregistros7 == 1){
-				
 				cout<<endl<<"Alumnos de 7mo grado:"<<endl;
 				cout<<"Nombre   Codigo   Curso   Nota1   Nota2   Nota3   Nota4   Nota5   Promedio"<<endl;
 				for(int i=0; i<cuantos_7; i++){
@@ -158,7 +156,7 @@ cout<<"--------------------------------------------------------------"<<endl;
 					cout<<endl;
 				}
 				for(int j=0; j<cuantos_7; j++){
-					promedio_curso7 = promedio_curso7 + noveno_grado_notas[j][5];
+					promedio_curso7 = promedio_curso7 + septimo_grado_notas[j][5];
 				}
 				cout<<"Promedio del Curso: "<<promedio_curso7 / cuantos_7<<endl;
 				mayor7=septimo_grado_notas[0][5];
@@ -179,11 +177,11 @@ cout<<"--------------------------------------------------------------"<<endl;
 					cout<<octavo_grado[i][1]<<"    ";
 					cout<<"8vo"<<"        ";
 					cout<<octavo_grado_notas[i][0]<<"    ";
-					cout<<octavo_grado_notas[i][1]<<"       ";
-					cout<<octavo_grado_notas[i][2]<<"       ";
-					cout<<octavo_grado_notas[i][3]<<"       ";
-					cout<<octavo_grado_notas[i][4]<<"       ";
-					cout<<octavo_grado_notas[i][5]<<"       ";
+					cout<<octavo_grado_notas[i][1]<<"      ";
+					cout<<octavo_grado_notas[i][2]<<"      ";
+					cout<<octavo_grado_notas[i][3]<<"      ";
+					cout<<octavo_grado_notas[i][4]<<"      ";
+					cout<<octavo_grado_notas[i][5]<<"      ";
 					cout<<endl;
 				}
 				for(int j=0; j<cuantos_8; j++){
@@ -208,11 +206,11 @@ cout<<"--------------------------------------------------------------"<<endl;
 					cout<<noveno_grado[i][1]<<"    ";
 					cout<<"9no"<<"        ";
 					cout<<noveno_grado_notas[i][0]<<"    ";
-					cout<<noveno_grado_notas[i][1]<<"       ";
-					cout<<noveno_grado_notas[i][2]<<"       ";
-					cout<<noveno_grado_notas[i][3]<<"       ";
-					cout<<noveno_grado_notas[i][4]<<"       ";
-					cout<<noveno_grado_notas[i][5]<<"       ";
+					cout<<noveno_grado_notas[i][1]<<"      ";
+					cout<<noveno_grado_notas[i][2]<<"      ";
+					cout<<noveno_grado_notas[i][3]<<"      ";
+					cout<<noveno_grado_notas[i][4]<<"      ";
+					cout<<noveno_grado_notas[i][5]<<"      ";
 					cout<<endl;
 				}
 				for(int j=0; j<cuantos_9; j++){
@@ -236,15 +234,18 @@ cout<<"--------------------------------------------------------------"<<endl;
 				if(septimo_grado_notas[mayor_index7][5]>mayor){
 					cout<<"El mejor estudiante del colegio es: "<<septimo_grado[mayor_index7][0]<<" con un promedio de: "<<septimo_grado_notas[mayor_index7][5]<<endl;
 				}else{
-					cout<<"El mejor estudiante del curso es: "<<octavo_grado[mayor_index8][0]<<" con un promedio de: "<<octavo_grado_notas[mayor_index8][5]<<endl;
+					cout<<"El mejor estudiante del colegio es: "<<octavo_grado[mayor_index8][0]<<" con un promedio de: "<<octavo_grado_notas[mayor_index8][5]<<endl;
 				}
 			}else if(septimo_grado_notas[mayor_index7][5]>mayor){
-				cout<<"El mejor estudiante del curso es: "<<septimo_grado[mayor_index7][0]<<" con un promedio de: "<<septimo_grado_notas[mayor_index7][5]<<endl;
+				cout<<"El mejor estudiante del colegio es: "<<septimo_grado[mayor_index7][0]<<" con un promedio de: "<<septimo_grado_notas[mayor_index7][5]<<endl;
 			}else{
-				cout<<"El mejor estudiante del curso es: "<<noveno_grado[mayor_index9][0]<<" con un promedio de: "<<noveno_grado_notas[mayor_index9][5]<<endl;
+				cout<<"El mejor estudiante del colegio es: "<<noveno_grado[mayor_index9][0]<<" con un promedio de: "<<noveno_grado_notas[mayor_index9][5]<<endl;
 			}
 			cout<<"Promedio general del colegio es: "<<(promedio_curso7/cuantos_7 + promedio_curso8/cuantos_8 + promedio_curso9/cuantos_9) / cuantoscursos<<endl;
 			cout<<"-------------------------------------------------------"<<endl<<endl;
+			
+			
+			cout<<endl<<endl<<"Finalizar...";
 		}	
 	}	
 	return 0;  
